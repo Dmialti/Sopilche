a9938967e8db6ed6477f75942939ee46f8ee6d59
anqiqhr <petroderlytsia@gmail.com>
2024-12-22 18:48:09 +0200

    Removed old script and objects, started from blank, created new script

---
+++ Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textLabel;
+
+    private void Start()
+    {
+        textLabel.text = "Çהאנמגא ןוס‏םט\nÍףקמ ןמûדנא‎ל))))))";
+    }
+}
